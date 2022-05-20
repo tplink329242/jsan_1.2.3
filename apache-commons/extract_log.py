23dabc345f734aaf09e5f52e7373791fbf9bffee
Guangbei <guangbei.yi@wsu.edu>
2022-05-19 21:42:03 -0700

    update script

--- apache-commons/extract_log.py
+++ apache-commons/extract_log.py
@@ -12,7 +12,8 @@ record_count = 0
 def record_logs(time_count = 0, log_names = "reduced.log"):
 
     print("Frequency is: " + time_count + " seconds")
-    log_names += "_reduced.log"
+    #log_names += "_reduced.log"
+    
     print("log name is: " + log_names)
 
     time_kick = int(time_count)
@@ -32,6 +33,7 @@ def record_logs(time_count = 0, log_names = "reduced.log"):
 
             if init_flag:
                 f_w = open(log_names, 'w')
+
                 with open(target_file, 'r', encoding='utf-8') as f:
 
                     print("Writing with init at: " + ticks)
@@ -44,7 +46,9 @@ def record_logs(time_count = 0, log_names = "reduced.log"):
                 init_flag = False
 
             else:
+
                 f_w = open(log_names, 'a')
+
                 with open(target_file, 'r', encoding='utf-8') as f:
 
                     print("Writing with not init at: " + ticks)
