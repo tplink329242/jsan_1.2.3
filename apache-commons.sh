5ac9be4b138d41db23d32247472be1bce54cf941
Guangbei <guangbei.yi@wsu.edu>
2022-05-19 21:49:11 -0700

    update script

--- apache-commons.sh
+++ apache-commons.sh
@@ -8,4 +8,4 @@ cd apache-commons/
 
 nohup sh run.sh > full.log 2>&1 &
 
-python extract_log.py 5 reduced.log
+python extract_log.py 5 $0
--- apache-commons/extract_log.py
+++ apache-commons/extract_log.py
@@ -3,16 +3,17 @@ import time
 import os
 import sys
 
-target_file = 'full.log'
-init_flag = True  
 #time_kick = 1800
 
 record_count = 0
 
 def record_logs(time_count = 0, log_names = "reduced.log"):
 
+    target_file = 'full.log'
+    init_flag = True  
+
     print("Frequency is: " + time_count + " seconds")
-    #log_names += "_reduced.log"
+    log_names += "_reduced.log"
 
     print("log name is: " + log_names)
 
@@ -23,74 +24,40 @@ def record_logs(time_count = 0, log_names = "reduced.log"):
 
         ticks = time.asctime( time.localtime(time.time()) )
 
-        if not os.path.exists(target_file):
-            print("No logs found... sleeping at: " + ticks)
-            time.sleep(time_kick)
-            continue
+        try:
+            #print("Entering try block..." + ticks)
 
-        if init_flag:
-            f_w = open(log_names, 'w')
+            if init_flag:
+                f_w = open(log_names, 'w')
 
-            with open(target_file, 'r', encoding='utf-8') as f:
+                with open(target_file, 'r', encoding='utf-8') as f:
 
-                print("Writing with init at: " + ticks)
+                    print("Writing with init at: " + ticks)
 
-                lines = f.readlines()
-                last_line = lines[-1]
-                f_w.write(ticks + " " + last_line + "\n")
-                record_count += 1
+                    lines = f.readlines()
+                    last_line = lines[-1]
+                    f_w.write(ticks + " " + last_line + "\n")
+                    record_count += 1
 
-            init_flag = False
+                init_flag = False
 
-        else:
+            else:
 
-            f_w = open(log_names, 'a')
+                f_w = open(log_names, 'a')
 
-            with open(target_file, 'r', encoding='utf-8') as f:
+                with open(target_file, 'r', encoding='utf-8') as f:
 
-                print("Writing with not init at: " + ticks)
+                    print("Writing with not init at: " + ticks)
 
-                lines = f.readlines()
-                last_line = lines[-1]
-                f_w.write(ticks + " " + last_line + "\n")
-                record_count += 1
+                    lines = f.readlines()
+                    last_line = lines[-1]
+                    f_w.write(ticks + " " + last_line + "\n")
+                    record_count += 1
 
-        f_w.close()
+            f_w.close()
 
-        # try:
-        #     #print("Entering try block..." + ticks)
-
-        #     if init_flag:
-        #         f_w = open(log_names, 'w')
-
-        #         with open(target_file, 'r', encoding='utf-8') as f:
-
-        #             print("Writing with init at: " + ticks)
-
-        #             lines = f.readlines()
-        #             last_line = lines[-1]
-        #             f_w.write(ticks + " " + last_line + "\n")
-        #             record_count += 1
-
-        #         init_flag = False
-
-        #     else:
-
-        #         f_w = open(log_names, 'a')
-
-        #         with open(target_file, 'r', encoding='utf-8') as f:
-
-        #             print("Writing with not init at: " + ticks)
-
-        #             lines = f.readlines()
-        #             last_line = lines[-1]
-        #             f_w.write(ticks + " " + last_line + "\n")
-        #             record_count += 1
-
-        #     f_w.close()
-
-        # except:
-        #     pass
+        except:
+            pass
 
         time.sleep(time_kick)
 
