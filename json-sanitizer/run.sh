c35578f7d03b4d754750cf1a037b1dbac3588277
Guangbei <guangbei.yi@wsu.edu>
2022-06-02 20:55:57 -0700

    add new drive

--- json-sanitizer/run.sh
+++ json-sanitizer/run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-./jazzer --cp=json-sanitizer.jar --autofuzz=com.google.json.JsonSanitizer::sanitize
+#./jazzer --cp=json-sanitizer.jar --autofuzz=com.google.json.JsonSanitizer::sanitize
+
+./jazzer --cp=json-sanitizer_Valid.jar --target_class="ValidJsonFuzzer" corpus
